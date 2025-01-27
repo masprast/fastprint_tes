937c11eb3da85f2f55d5925970937bfc73ffa74f
masprast <henry.prasetya.kurniawan@gmail.com>
2025-01-27 11:07:54 +0700

    deployed db to cloud

--- backend_tes/urls.py
+++ backend_tes/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('katalog.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("katalog.urls")),
+    path("initdb/", include("utils.mengisi_db")),
     # path('rest/',include('rest_framework.urls'))
 ]
--- katalog/urls.py
+++ katalog/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r"produk", views.ListProduk, basename="produk")
 router.register("kategori", views.ListKategori)
 router.register("status", views.ListStatus)
-router.register("initdb", views.InitDB)
+# router.register("initdb", views.InitDB)
 
 urlpatterns = [
     path("katalog/", include(router.urls)),
